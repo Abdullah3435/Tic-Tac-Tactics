6c55349cdda37966fa80cd65f95ea386bb547c86
Musab1Blaser <musab16000@gmail.com>
2025-04-24 09:24:16 +0500

    firebase key issue fixed

--- Backend/firebase_init.py
+++ Backend/firebase_init.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials
 
 # Initialize the Firebase Admin SDK
 def initfirebase():
-    cred = credentials.Certificate('tic-tac-tactics-firebase-adminsdk-fbsvc-94e569130e.json')
+    cred = credentials.Certificate('/home/musab/Habib_University/6th Semester/SE/project/Tic-Tac-Tactics/Backend/tic-tac-tactics-firebase-adminsdk-fbsvc-4634b03bd5.json')
     print("firebase init")
     firebase_admin.initialize_app(cred,{'databaseURL': "https://tic-tac-tactics-default-rtdb.firebaseio.com"})
     print("firebase init done")
